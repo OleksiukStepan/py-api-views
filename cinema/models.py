5d1f33a74e4e46987f25b2089338ae40bfc9aff1
Stepan <oleksiukstepann@gmail.com>
2024-09-29 18:47:05 +0300

    add help_text to duration field

--- cinema/models.py
+++ cinema/models.py
@@ -28,7 +28,7 @@ class CinemaHall(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    duration = models.IntegerField()
+    duration = models.IntegerField(help_text="Duration in minutes")
     actors = models.ManyToManyField(Actor, related_name="movies")
     genres = models.ManyToManyField(Genre, related_name="movies")
 
